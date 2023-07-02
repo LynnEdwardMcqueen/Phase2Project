9884796d800d9814bc8cfd6e40fffd180fbdc990
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-07-02 09:37:20 -0600

    Adds championMVP to the input form

--- src/components/ChampionshipDataForm.js
+++ src/components/ChampionshipDataForm.js
@@ -3,6 +3,8 @@ import { useState} from "react";
 
 function ChampionshipDataForm({titleToEdit, formVisibility}) {
   let [championTeam, setChampionTeam] = useState(titleToEdit ? titleToEdit.team : "")
+  let [championYear, setChampionYear] = useState(titleToEdit ? titleToEdit.year : "")
+  let [championMVP, setChampionMVP] = useState(titleToEdit ? titleToEdit.mvp : "")
   if (formVisibility === "visible") {
       console.log("titleToEdit = ", titleToEdit);
       debugger
@@ -11,6 +13,10 @@ function ChampionshipDataForm({titleToEdit, formVisibility}) {
     <form style = {{visibility: `${formVisibility}`}}>
        <label for = "team">Team</label>
        <input id= "team" type="text" value = {championTeam} /><br />
+       <label for = "year">Year</label>
+       <input id = "year" type = "number" value = {championYear} /> <br />
+       <label for = "mvp">MVP</label>
+       <input id = "mvp" type = "text" />
     </form>
        
   )
