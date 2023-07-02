7a2b6b150d3c040a7412a0e7d2faed9c64f2ef8f
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-07-02 09:23:24 -0600

    Creates first input field in ChampionshipDataForm.  Had to change to controlled input.

--- src/components/ChampionshipDataForm.js
+++ src/components/ChampionshipDataForm.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useState} from "react";
+
 function ChampionshipDataForm({titleToEdit, formVisibility}) {
-    if (formVisibility === "visible") {
-        console.log("titleToEdit = ", titleToEdit);
-        debugger
-    }
-    return (
-        <p style = {{visibility: `${formVisibility}`}}> Here in championshipDataForm </p>
-    )
+  let [championTeam, setChampionTeam] = useState(titleToEdit ? titleToEdit.team : "")
+  if (formVisibility === "visible") {
+      console.log("titleToEdit = ", titleToEdit);
+      debugger
+  }
+  return (
+    <form style = {{visibility: `${formVisibility}`}}>
+       <input type="text" value = {championTeam} /><br />
+    </form>
+       
+  )
 }
 
 export default ChampionshipDataForm
