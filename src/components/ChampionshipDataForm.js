8694539830ff084acd43ee886b1f5563843a9e68
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-07-02 08:32:01 -0600

    ChampionshipDataForm visibility not handled via state and prop!

--- src/components/ChampionshipDataForm.js
+++ src/components/ChampionshipDataForm.js
@@ -1,8 +1,8 @@
 import React from "react";
 function ChampionshipDataForm({formVisibility}) {
     return (
-        <p style = {`visibility: ${formVisibility}`}> Here in championshipDataForm </p>
+        <p style = {{visibility: `${formVisibility}`}}> Here in championshipDataForm </p>
     )
 }
 
-export default championshipDataForm
+export default ChampionshipDataForm
--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useState} from "react";
 import SelectTeam from "./SelectTeam.js"
+import ChampionshipDataForm from "./ChampionshipDataForm.js"
 
 function Edit( {titles}) {
   let [idForEdit, updateEditId] = useState(0)
+  let [inputFormVisibility, updateInputFormVisibility] = useState("hidden")
 
   function recordEditId(event) {
     console.log("recordEditId = ", Number(event.target.value))
@@ -14,14 +16,18 @@ function Edit( {titles}) {
     event.preventDefault();
     console.log("Edit team submit received")
     console.log("id for edit = ", idForEdit)
+    updateInputFormVisibility("visible")
     debugger
   }
+
+
 
 
   return (
     <div className="Filter">
       <p>Edit</p>
       <SelectTeam titlesList = {titles} actionName = "Edit" onSelectValueChange = {recordEditId} onSubmitHandler = {editTeamSubmitEvent} />
+      <ChampionshipDataForm  formVisibility = {inputFormVisibility} />
     </div>
   )
 
